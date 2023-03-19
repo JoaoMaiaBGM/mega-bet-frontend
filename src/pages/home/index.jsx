ea57bd7b0e8773e55ed36c8cfca72227cd0e2d98
João Maia <joaocarlosbgm@gmail.com>
2023-03-18 23:23:06 -0300

    refactor:create component for numbers list + change background color on home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,95 +1,20 @@
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
-import {
-  Box,
-  Button,
-  Divider,
-  Heading,
-  List,
-  ListIcon,
-  ListItem,
-  Stack,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { FaCheckCircle } from "react-icons/fa";
-import { handleBet } from "../../utils/bet.util";
-import { useContext } from "react";
-import { ProfileContext } from "../../contexts/user.context";
-
-const PackageTier = () => {
-  const { bet, setBet } = useContext(ProfileContext);
-
-  return (
-    <form
-      onSubmit={async (eve) => {
-        eve.preventDefault();
-        const response = await handleBet();
-        if (response?.message == "success") {
-          setBet(response.data);
-        }
-      }}
-    >
-      <Stack
-        p={3}
-        py={3}
-        justifyContent={{
-          base: "flex-start",
-          md: "space-between",
-        }}
-        direction={{
-          base: "column",
-          md: "row",
-        }}
-        alignItems={{ md: "center" }}
-      >
-        <Heading
-          size={"md"}
-          direction={{ base: "column", md: "row" }}
-          textAlign={"center"}
-        >
-          Usuário:
-          <Text fontSize={"md"}>{bet.user}</Text>
-        </Heading>
-        {bet === false ? (
-          <Text>Você ainda não gerou os números</Text>
-        ) : (
-          <List spacing={4} textAlign="center" fontSize={"larger"}>
-            {bet.numbers.map((num, id) => (
-              <ListItem key={id}>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                {num}
-              </ListItem>
-            ))}
-          </List>
-        )}
-
-        <Heading
-          size={"md"}
-          direction={{ base: "column", md: "row" }}
-          textAlign={"center"}
-        >
-          Data:
-          <Text fontSize={"md"}>{bet.createdAt}</Text>
-        </Heading>
-        <Button
-          size="md"
-          color={useColorModeValue("purple.600", "colorTextDark")}
-          bgColor={useColorModeValue("gray.300", "gray.300")}
-          type={"submit"}
-        >
-          Gerar
-        </Button>
-      </Stack>
-    </form>
-  );
-};
+import { BetsDashboard } from "../../components/betDashboard";
+import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
     <>
       <Header></Header>
-      <Box py={6} px={5} minH={"md"} maxW={"80vw"} margin={"0 auto"}>
+      <Box
+        py={6}
+        px={5}
+        minH={"md"}
+        maxW={"100vw"}
+        margin={"0 auto"}
+        bg={"facebook.500"}
+      >
         <Stack spacing={4} width={"100%"} direction={"column"}>
           <Stack
             p={5}
@@ -100,7 +25,7 @@ export const Home = () => {
             }}
             direction={{
               base: "column",
-              md: "row",
+              md: "column",
             }}
           >
             <Stack
@@ -110,9 +35,9 @@ export const Home = () => {
               }}
               textAlign={"center"}
             >
-              <Heading size={"lg"}>
+              <Heading size={"lg"} color={"whiteAlpha.800"}>
                 Gere seus números para a{" "}
-                <Text color="purple.400">Mega-sena</Text>
+                <Text color="whatsapp.500">Mega-sena</Text>
               </Heading>
             </Stack>
             <Stack
@@ -121,14 +46,13 @@ export const Home = () => {
                 md: "60%",
               }}
             >
-              <Text textAlign={"center"}>
+              <Text textAlign={"center"} fontSize={15} color={"whiteAlpha.800"}>
                 Clique no botão abaixo, 'gerar'. Os números aparecerão na lista.
               </Text>
             </Stack>
           </Stack>
-          <Divider />
-          <PackageTier />
-          <Divider />
+
+          <BetsDashboard />
         </Stack>
       </Box>
       <Footer />
